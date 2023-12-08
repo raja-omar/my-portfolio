2d07174c2a6039fb8a4738b0653d65e98cb790c3
Raja Muhammed Omar <139909039+raja-omar@users.noreply.github.com>
2023-12-07 22:36:24 -0700

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
+  base: "/my-portfolio/",
+
   server: {
     host: true,
   },
